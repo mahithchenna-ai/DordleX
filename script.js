16d784bbc00a412684526fa0bb3473857a73a49b
Chenna Mahith <your-email@example.com>
2026-04-01 22:56:05 +0530

    fetching meanings

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ async function fetchWords() {
 
   return words.slice(0, 2);
 }
+
+// Fetch meaning of a word
+async function fetchMeaning(word) {
+  const response = await fetch(
+    "https://api.dictionaryapi.dev/api/v2/entries/en/" + word
+  );
+
+  const data = await response.json();
+
+  let meaning = "Meaning not found";
+
+  if (
+    data.length > 0 &&
+    data[0].meanings &&
+    data[0].meanings.length > 0 &&
+    data[0].meanings[0].definitions &&
+    data[0].meanings[0].definitions.length > 0
+  ) {
+    meaning = data[0].meanings[0].definitions[0].definition;
+  }
+
+  return meaning;
+}
